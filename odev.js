99662c754b9a213ff6f18f1bb21627f7e36c939a
ecem <115075925+ecemnsntcglu@users.noreply.github.com>
2024-06-17 14:58:01 +0300

    Create odev.js

---
+++ odev.js
@@ -0,0 +1,16 @@
+let sayi = 15;
+
+if (sayi < 10) {
+  console.log("Sayı 10'dan küçüktür.");
+} else {
+  console.log("Sayı 10 veya daha büyüktür.");
+}
+
+
+let sonuc = (5 * 4) + (10 / 3) - 1;
+console.log("İşlem Sonucu:", sonuc);
+
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
